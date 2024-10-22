e67ccfff1f8f0361df679ce4fe962da0c17ec152
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2024-10-22 16:45:26 +0100

    feat: Add test wrapper app

---
+++ modules/test-wrapper/src/main/java/uk/gov/onelogin/criorchestrator/testwrapper/MainActivity.kt
@@ -0,0 +1,47 @@
+package uk.gov.onelogin.criorchestrator.testwrapper
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import uk.gov.onelogin.criorchestrator.testwrapper.ui.theme.TestWrapperTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TestWrapperTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HelloWorld(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HelloWorld(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.hello_world_message),
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloWorldPreview() {
+    TestWrapperTheme {
+        HelloWorld()
+    }
+}
