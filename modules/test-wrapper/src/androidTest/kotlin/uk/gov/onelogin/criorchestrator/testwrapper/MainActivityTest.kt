fd211f3928526c2451332a14a95d57acba459382
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2024-11-05 10:50:16 +0000

    refactor: Update source directory name from java to kotlin

--- modules/test-wrapper/src/androidTest/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/MainActivityTest.kt
+++ modules/test-wrapper/src/androidTest/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/MainActivityTest.kt
@@ -6,7 +6,6 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
 
--- modules/test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/MainActivity.kt
+++ modules/test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/MainActivity.kt
@@ -2,8 +2,8 @@ package uk.gov.onelogin.criorchestrator.testwrapper
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
