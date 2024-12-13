b17478461f077efd4d99eec49990afdee9030c3f
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2024-12-13 16:03:16 +0000

    feat: Add workflow to expose signed bundle
    - Create temporary workflow to generate bundle
      signed by workflow, and expose it at end of workflow
    
    Resolves: DCMAW-10666

--- modules/test-wrapper/build.gradle.kts
+++ modules/test-wrapper/build.gradle.kts
@@ -6,6 +6,28 @@ plugins {
     id("uk.gov.onelogin.criorchestrator.android-app-config")
 }
 
+android {
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+
+    dataBinding { enable = true }
+    productFlavors {
+        flavorDimensions.add("env")
+
+        listOf(
+            "build",
+            "staging",
+        ).forEach { flavourString ->
+            create(flavourString) {
+                dimension = "env"
+                applicationIdSuffix = ".$flavourString"
+            }
+        }
+    }
+}
+
 configure<ApplicationExtension> {
     setApplicationId(suffix = ".testwrapper")
     setNamespace(suffix = ".testwrapper")
