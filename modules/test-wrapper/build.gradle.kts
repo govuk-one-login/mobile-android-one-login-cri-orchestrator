f44a543d4188a6ccf43a24bfc24fd247906d0658
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-01-10 11:47:51 +0000

    build: Move environment configuration to plugin
    Co-authored-by: Jackson <152998610+JacksonJ2W@users.noreply.github.com>

--- modules/test-wrapper/build.gradle.kts
+++ modules/test-wrapper/build.gradle.kts
@@ -1,35 +1,14 @@
 import com.android.build.api.dsl.ApplicationExtension
+import uk.gov.onelogin.criorchestrator.extensions.configureEnvironmentFlavors
 import uk.gov.onelogin.criorchestrator.extensions.setApplicationId
 import uk.gov.onelogin.criorchestrator.extensions.setNamespace
-import uk.gov.pipelines.extensions.ProjectExtensions.versionName
 
 plugins {
     id("uk.gov.onelogin.criorchestrator.android-app-config")
 }
 
-android {
-    buildFeatures {
-        buildConfig = true
-        compose = true
-    }
-
-    dataBinding { enable = true }
-    productFlavors {
-        flavorDimensions.add("env")
-
-        listOf(
-            "build",
-            "staging",
-        ).forEach { flavourString ->
-            create(flavourString) {
-                dimension = "env"
-                applicationIdSuffix = ".$flavourString"
-            }
-        }
-    }
-}
-
 configure<ApplicationExtension> {
     setApplicationId(suffix = ".testwrapper")
     setNamespace(suffix = ".testwrapper")
+    configureEnvironmentFlavors()
 }
