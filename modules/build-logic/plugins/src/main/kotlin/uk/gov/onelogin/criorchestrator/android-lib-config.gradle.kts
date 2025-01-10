07046b72275e1909f1a26376fae46823b6af4956
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-01-10 14:54:18 +0000

    refactor: Simplify `sdk:build.gradle` file
    - Remove currently unneeded code to reduce PR size
    - Use setup already existing in build-logic plugins extensions
    
    DCMAW-10666
    
    Co-authored-by: Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>

--- modules/build-logic/plugins/src/main/kotlin/uk/gov/onelogin/criorchestrator/android-lib-config.gradle.kts
+++ modules/build-logic/plugins/src/main/kotlin/uk/gov/onelogin/criorchestrator/android-lib-config.gradle.kts
@@ -1,9 +1,35 @@
 package uk.gov.onelogin.criorchestrator
 
+import com.android.build.api.dsl.LibraryExtension
 import org.gradle.accessors.dm.LibrariesForLibs
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import uk.gov.onelogin.criorchestrator.extensions.androidTestDependencies
+import uk.gov.onelogin.criorchestrator.extensions.setJavaVersion
+import uk.gov.onelogin.criorchestrator.extensions.testDependencies
+import uk.gov.onelogin.criorchestrator.extensions.uiDependencies
 
 //https://github.com/gradle/gradle/issues/15383
 val libs = the<LibrariesForLibs>()
+
+listOf(
+    "uk.gov.pipelines.android-lib-config"
+).forEach {
+    project.plugins.apply(it)
+}
+
+configure<LibraryExtension>{
+    setJavaVersion()
+}
+
+configure<KotlinAndroidProjectExtension> {
+    setJavaVersion()
+}
+
+dependencies{
+    androidTestDependencies(libs)
+    testDependencies(libs)
+    uiDependencies(libs)
+}
 
 listOf(
     libs.plugins.android.library,
--- modules/sdk/build.gradle.kts
+++ modules/sdk/build.gradle.kts
@@ -1,69 +1,19 @@
 import com.android.build.api.dsl.LibraryExtension
 import uk.gov.onelogin.criorchestrator.extensions.setNamespace
-import uk.gov.pipelines.config.ApkConfig
 
 plugins {
     id("uk.gov.onelogin.criorchestrator.android-lib-config")
-    // TODO remove
-    id("uk.gov.pipelines.android-lib-config")
 }
 
 configure<LibraryExtension> {
     setNamespace(suffix = ".sdk")
 }
 
-android {
-    buildFeatures {
-        buildConfig = true
-    }
-
-    val apkConfig: ApkConfig by project.rootProject.extra
-    defaultConfig {
-        compileSdk = apkConfig.sdkVersions.compile
-        minSdk = apkConfig.sdkVersions.minimum
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
 dependencies {
     listOf(
-        libs.androidx.espresso.core,
         libs.androidx.test.espresso.accessibility,
-        libs.androidx.junit,
     ).forEach {
         androidTestImplementation(it)
-    }
-
-    androidTestUtil(libs.androidx.test.orchestrator)
-
-    listOf(
-        libs.androidx.appcompat,
-        libs.androidx.core.ktx,
-        libs.material,
-    ).forEach {
-        implementation(it)
-    }
-
-    listOf(
-        libs.junit,
-    ).forEach {
-        testImplementation(it)
     }
 }
 
