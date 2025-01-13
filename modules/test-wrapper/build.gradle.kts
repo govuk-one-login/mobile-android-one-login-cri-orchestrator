3159bd432ab73f6d9a52611c1438a4cf17af3cba
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-01-13 16:34:04 +0000

    wip: troubleshooting targetSdk too low issue
    Resolves: DCMAW-10666
    
    Co-authored-by: Delphine <128810121+de1phine@users.noreply.github.com>

--- modules/test-wrapper/build.gradle.kts
+++ modules/test-wrapper/build.gradle.kts
@@ -2,6 +2,7 @@ import com.android.build.api.dsl.ApplicationExtension
 import uk.gov.onelogin.criorchestrator.extensions.configureEnvironmentFlavors
 import uk.gov.onelogin.criorchestrator.extensions.setApplicationId
 import uk.gov.onelogin.criorchestrator.extensions.setNamespace
+import uk.gov.pipelines.config.ApkConfig
 
 plugins {
     id("uk.gov.onelogin.criorchestrator.android-app-config")
@@ -12,3 +13,15 @@ configure<ApplicationExtension> {
     setNamespace(suffix = ".testwrapper")
     configureEnvironmentFlavors()
 }
+
+val apkConfig by rootProject.extra(
+    object: ApkConfig {
+        override val applicationId: String = "uk.gov.onelogin.criorchestrator.testwrapper"
+        override val debugVersion: String = "DEBUG_VERSION"
+        override val sdkVersions = object: ApkConfig.SdkVersions {
+            override val minimum = 29
+            override val target = 34
+            override val compile = 35
+        }
+    }
+)
