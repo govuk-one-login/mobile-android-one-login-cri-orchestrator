2df60fff463d28462ec0acb18024a37a64801f0a
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-01-15 10:34:40 +0000

    chore: update naming to match tech design

--- modules/sdk/src/main/java/uk/gov/onelogin/criorchestrator/sdk/resume/IDCheckCard.kt
+++ modules/sdk/src/main/java/uk/gov/onelogin/criorchestrator/sdk/resume/IDCheckCard.kt
@@ -1,14 +1,20 @@
 package uk.gov.onelogin.criorchestrator.sdk.resume
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import uk.gov.android.ui.componentsv2.ContentTileParameters
 import uk.gov.android.ui.componentsv2.GdsContentTile
 import uk.gov.android.ui.theme.m3.GdsTheme
+import uk.gov.onelogin.criorchestrator.sdk.CriOrchestrator
 import uk.gov.onelogin.criorchestrator.sdk.R
 
 @Composable
-fun IDCheckCard() {
+fun IDCheckCard(
+    component: CriOrchestrator,
+    modifier: Modifier
+) {
     GdsContentTile(
         parameters = ContentTileParameters(
             title = R.string.start_id_check_title,
@@ -24,6 +30,9 @@ fun IDCheckCard() {
 @PreviewLightDark
 internal fun ContentTilePreview() {
     GdsTheme {
-        IDCheckCard()
+        IDCheckCard(
+            component = CriOrchestrator(),
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
