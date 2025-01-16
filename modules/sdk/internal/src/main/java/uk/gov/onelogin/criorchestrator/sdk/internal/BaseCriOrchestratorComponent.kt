e9ace02f794ee4d64f436721776639897a9aab7f
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-01-16 09:44:26 +0000

    build: add anvil dependency
    Resolves: DCMAW-10854

--- modules/sdk/internal/src/main/java/uk/gov/onelogin/criorchestrator/sdk/internal/BaseCriOrchestratorComponent.kt
+++ modules/sdk/internal/src/main/java/uk/gov/onelogin/criorchestrator/sdk/internal/BaseCriOrchestratorComponent.kt
@@ -3,5 +3,4 @@ package uk.gov.onelogin.criorchestrator.sdk.internal
 import uk.gov.onelogin.criorchestrator.libraries.di.ActivityScope
 
 @ActivityScope
-interface BaseCriOrchestratorComponent {
-}
+interface BaseCriOrchestratorComponent
