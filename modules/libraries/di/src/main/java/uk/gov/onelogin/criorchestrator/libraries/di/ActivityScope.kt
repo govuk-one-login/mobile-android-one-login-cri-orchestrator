d40958383fc59541a7e49f526316773082f865ee
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-01-17 12:36:03 +0000

    docs: document APIs
    Resolves: DCMAW-10843
    
    Co-authored-by: Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>

--- modules/libraries/di/src/main/java/uk/gov/onelogin/criorchestrator/libraries/di/ActivityScope.kt
+++ modules/libraries/di/src/main/java/uk/gov/onelogin/criorchestrator/libraries/di/ActivityScope.kt
@@ -2,6 +2,9 @@ package uk.gov.onelogin.criorchestrator.libraries.di
 
 import javax.inject.Scope
 
+/**
+ * Dagger scope intended for use at Activity level.
+ */
 @Scope
 @MustBeDocumented
 @Retention(AnnotationRetention.RUNTIME)
--- modules/sdk/shared-api/src/main/java/uk/gov/onelogin/criorchestrator/sdk/sharedapi/CriOrchestratorComponent.kt
+++ modules/sdk/shared-api/src/main/java/uk/gov/onelogin/criorchestrator/sdk/sharedapi/CriOrchestratorComponent.kt
@@ -3,5 +3,9 @@ package uk.gov.onelogin.criorchestrator.sdk.sharedapi
 import com.squareup.anvil.annotations.ContributesTo
 import uk.gov.onelogin.criorchestrator.libraries.di.CriOrchestratorScope
 
+/**
+ * Public-facing abstraction that hides the internal Dagger component `BaseCriOrchestratorComponent`
+ * and provides no functionality.
+ */
 @ContributesTo(CriOrchestratorScope::class)
 interface CriOrchestratorComponent
