88759ff091d85f8b9f05f4fd08fffe3536654033
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-01-17 13:55:48 +0000

    chore: address comments about unit tests
    - remove KDoc on tests
    - update DI tests from `Example` to `Stub`
    
    Resolves: DCMAW-10843

--- modules/libraries/di/src/androidTest/java/uk/gov/onelogin/criorchestrator/libraries/di/StubInstrumentationTest.kt
+++ modules/libraries/di/src/androidTest/java/uk/gov/onelogin/criorchestrator/libraries/di/StubInstrumentationTest.kt
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class StubInstrumentationTest {
     @Test
     fun stubInstrumentationTest() {
         assertEquals(
