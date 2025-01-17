f7cb9942401ee7cc756963c54bba2497f8578ed0
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-01-17 13:55:48 +0000

    chore: address comments about unit tests
    - remove KDoc on tests
    - update DI tests from `Example` to `Stub`
    
    Resolves: DCMAW-10843

--- modules/libraries/di/src/test/java/uk/gov/onelogin/criorchestrator/libraries/di/StubUnitTest.kt
+++ modules/libraries/di/src/test/java/uk/gov/onelogin/criorchestrator/libraries/di/StubUnitTest.kt
@@ -3,12 +3,7 @@ package uk.gov.onelogin.criorchestrator.libraries.di
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+class StubUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
