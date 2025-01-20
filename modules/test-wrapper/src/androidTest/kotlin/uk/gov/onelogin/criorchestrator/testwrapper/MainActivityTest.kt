fc2478d9fa25cedffc50c96411eec33e8a96bc9b
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-01-20 17:00:57 +0000

    test: add preview test

--- modules/test-wrapper/src/androidTest/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/MainActivityTest.kt
+++ modules/test-wrapper/src/androidTest/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/MainActivityTest.kt
@@ -1,21 +1,35 @@
 package uk.gov.onelogin.criorchestrator.testwrapper
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
     fun itLaunches() {
         launchActivity<MainActivity>().use {
             onView(withId(android.R.id.content))
                 .check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun previewTest() {
+        composeTestRule.setContent {
+            HelloWorldPreview()
         }
     }
 }
