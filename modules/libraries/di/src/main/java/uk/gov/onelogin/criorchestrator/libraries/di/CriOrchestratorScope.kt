c22b5ca38ec6eeae0b362d9dc54ee999a6230298
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-01-20 08:44:43 +0000

    chore: rename `src/java` to `src/kotlin`
    - reduce confusion about language of files
    - sort settings.gradle.kts modules alphabetically
    
    Resolves: DCMAW-10843

--- modules/libraries/di/src/main/java/uk/gov/onelogin/criorchestrator/libraries/di/CriOrchestratorScope.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.onelogin.criorchestrator.libraries.di
-
-/**
- * Anvil scope used to help Anvil make a connection between the Dagger component and which Dagger
- * modules and other component interfaces to include.
- */
-abstract class CriOrchestratorScope private constructor()
