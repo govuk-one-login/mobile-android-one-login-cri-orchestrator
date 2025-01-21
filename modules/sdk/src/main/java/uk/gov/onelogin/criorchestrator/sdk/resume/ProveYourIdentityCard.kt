ef577d50556a5acc14439d98b96957aa1dd69d9d
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-01-21 09:51:19 +0000

    chore: update UI module dependency

--- modules/sdk/src/main/java/uk/gov/onelogin/criorchestrator/sdk/resume/ProveYourIdentityCard.kt
+++ modules/sdk/src/main/java/uk/gov/onelogin/criorchestrator/sdk/resume/ProveYourIdentityCard.kt
@@ -3,9 +3,9 @@ package uk.gov.onelogin.criorchestrator.sdk.resume
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import uk.gov.android.ui.componentsv2.ContentTileParameters
-import uk.gov.android.ui.componentsv2.GdsContentTile
+import uk.gov.android.ui.componentsv2.GdsCard
 import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.onelogin.criorchestrator.sdk.CriOrchestrator
 import uk.gov.onelogin.criorchestrator.sdk.R
@@ -13,24 +13,24 @@ import uk.gov.onelogin.criorchestrator.sdk.R
 @Composable
 fun ProveYourIdentityCard(
     @Suppress("UnusedParameter") component: CriOrchestrator,
-    @Suppress("UnusedParameter") modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier,
 ) {
-    GdsContentTile(
-        parameters = ContentTileParameters(
-            title = R.string.start_id_check_title,
-            body = R.string.start_id_check_content,
-            text = R.string.start_id_check_primary_button,
-        ),
-    ) {
-        // Identity Proofing and Verification (IPV)
-        // display the ID Check Dialog:
-        // DCMAW-10056: Android | Resume IPV | Create 'Start ID Check' modal UI
-    }
+    GdsCard(
+        title = stringResource(R.string.start_id_check_title),
+        body = stringResource(R.string.start_id_check_content),
+        buttonText = stringResource(R.string.start_id_check_primary_button),
+        modifier = modifier,
+        onClick = {
+            // Identity Proofing and Verification (IPV)
+            // display the ID Check Dialog:
+            // DCMAW-10056: Android | Resume IPV | Create 'Start ID Check' modal UI
+        }
+    )
 }
 
 @Composable
 @PreviewLightDark
-internal fun ContentTilePreview() {
+internal fun ProveYourIdentityCardPreview() {
     GdsTheme {
         ProveYourIdentityCard(
             component = CriOrchestrator(),
