f695f185a348fe220f5c202f98417f0339dfb9d8
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-01-21 11:47:21 +0000

    fix: rebase issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.anvil) apply false
     id("uk.gov.pipelines.android-root-config")
 }
 
