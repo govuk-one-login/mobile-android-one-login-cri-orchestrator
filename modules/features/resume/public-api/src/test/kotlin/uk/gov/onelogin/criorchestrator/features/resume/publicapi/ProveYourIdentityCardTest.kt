80e97f8c124c4bcff73cb670c028dbb5cb75e2bb
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-01-22 07:47:55 +0000

    test: alternative approach to testing ProveYourIdentityCard

--- modules/features/resume/public-api/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/resume/publicapi/ProveYourIdentityCardTest.kt
+++ modules/features/resume/public-api/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/resume/publicapi/ProveYourIdentityCardTest.kt
@@ -5,50 +5,49 @@ import androidx.compose.ui.Modifier
 import app.cash.molecule.RecompositionMode
 import app.cash.molecule.moleculeFlow
 import app.cash.turbine.test
-import dagger.Binds
 import dagger.Component
 import dagger.Module
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertInstanceOf
-import uk.gov.onelogin.criorchestrator.features.resume.internal.ProveYourIdentityEntryPointsImpl
 import uk.gov.onelogin.criorchestrator.features.resume.internalapi.ProveYourIdentityEntryPoints
 import uk.gov.onelogin.criorchestrator.features.resume.internalapi.ProveYourIdentityEntryPointsComponent
 import uk.gov.onelogin.criorchestrator.sdk.sharedapi.CriOrchestratorComponent
 
 @Module
-abstract class TestModule {
-    @Binds
-    abstract fun bindCriOrchestratorComponent(
-        mockComponent: MockCriOrchestratorComponent
-    ): TestCriOrchestratorComponent
-}
+abstract class TestModule
 
 @Component(modules = [TestModule::class])
-interface TestCriOrchestratorComponent: CriOrchestratorComponent
+interface TestProveYourIdentityCard
 
-
-class MockCriOrchestratorComponent:
-    TestCriOrchestratorComponent,
-    ProveYourIdentityEntryPointsComponent {
-    override fun proveYourIdentityEntryPoints(): ProveYourIdentityEntryPoints {
-        TODO("Not yet implemented")
-    }
+@Composable
+fun mockCard(): TestProveYourIdentityCard {
+    return DaggerTestProveYourIdentityCard.create()
 }
 
+class MockCriOrchestratorComponent: CriOrchestratorComponent, ProveYourIdentityEntryPointsComponent {
+    class MockProveYourIdentityEntryPoints: ProveYourIdentityEntryPoints {
+        @Composable
+        override fun ProveYourIdentityCard(modifier: Modifier) {
+            mockCard()
+        }
+    }
 
+    override fun proveYourIdentityEntryPoints(): ProveYourIdentityEntryPoints {
+        return MockProveYourIdentityEntryPoints()
+    }
+}
 
 class ProveYourIdentityCardTest {
     @Test
     fun addition_isCorrect() = runTest {
         moleculeFlow(RecompositionMode.Immediate) {
             ProveYourIdentityCard(
-                component = DaggerTestCriOrchestratorComponent.create(),
+                component = MockCriOrchestratorComponent(),
                 modifier = Modifier
             )
         }.test {
-            assertInstanceOf<ProveYourIdentityEntryPointsImpl>(awaitItem())
+            assertInstanceOf<TestProveYourIdentityCard>(awaitItem())
             cancel()
         }
     }
