8f53f98fa5f3d79ee8391b4c62f57c3c1a2e696e
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-01-28 08:47:10 +0000

    feat: DCMAW-10108: Firebase Integration (No Consent Mechanism) (#87)
    Co-authored-by: de1phine <delphine.mubiligi@digital.cabinet-office.gov.uk>
    Co-authored-by: Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.anvil) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.google.services) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.ksp) apply false
---
+++ test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/logging/HomeScreenViewEvent.kt
@@ -0,0 +1,13 @@
+package uk.gov.onelogin.criorchestrator.testwrapper.logging
+
+import uk.gov.logging.api.analytics.AnalyticsEvent
+import uk.gov.logging.api.analytics.parameters.ScreenViewParameters
+
+internal fun homeScreenViewEvent(obj: Any) =
+    ScreenViewParameters(
+        clazz = obj::class.java.simpleName,
+        name = "TestWrapper",
+        title = "Test Wrapper app",
+    ).let {
+        AnalyticsEvent.screenView(it)
+    }
