28cc3b6dbcdcea7d44cc5ce8285f901c96073789
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-01-30 11:52:11 +0000

    feat: add analytics to ProveYourIdentityViewModel

--- features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/ProveYourIdentityComponent.kt
+++ features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/ProveYourIdentityComponent.kt
@@ -2,7 +2,6 @@ package uk.gov.onelogin.criorchestrator.features.resume.internal
 
 import androidx.lifecycle.ViewModelProvider
 import com.squareup.anvil.annotations.ContributesTo
-import uk.gov.logging.api.analytics.logging.AnalyticsLogger
 import uk.gov.onelogin.criorchestrator.libraries.di.ActivityScope
 import uk.gov.onelogin.criorchestrator.libraries.di.CriOrchestratorScope
 import javax.inject.Named
@@ -11,7 +10,5 @@ import javax.inject.Named
 @ContributesTo(CriOrchestratorScope::class)
 interface ProveYourIdentityComponent {
     @Named(ProveYourIdentityViewModelModule.FACTORY_NAME)
-    fun proveIdentityViewModelFactory(
-       // analyticsLogger: AnalyticsLogger,
-    ): ViewModelProvider.Factory
+    fun proveIdentityViewModelFactory(): ViewModelProvider.Factory
 }
--- features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/ProveYourIdentityViewModelModule.kt
+++ features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/ProveYourIdentityViewModelModule.kt
@@ -1,7 +1,6 @@
 package uk.gov.onelogin.criorchestrator.features.resume.internal
 
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.squareup.anvil.annotations.ContributesTo
@@ -19,13 +18,14 @@ object ProveYourIdentityViewModelModule {
     @Provides
     @Named(FACTORY_NAME)
     fun provideFactory(
-        analyticsLogger: AnalyticsLogger
+        analyticsLogger: AnalyticsLogger,
+        resourceProvider: ResourceProvider,
     ): ViewModelProvider.Factory =
         viewModelFactory {
             initializer {
                 ProveYourIdentityViewModel(
                     analyticsLogger = analyticsLogger,
-                    savedStateHandle = createSavedStateHandle(),
+                    resourceProvider = resourceProvider,
                 )
             }
         }
