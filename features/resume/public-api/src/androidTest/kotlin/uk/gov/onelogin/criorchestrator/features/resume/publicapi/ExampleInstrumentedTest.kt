a704bd9037b63b273da6b9334a7825b16b706e1c
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-02-03 17:14:44 +0000

    test: Ignore placeholder tests

--- features/resume/public-api/src/androidTest/kotlin/uk/gov/onelogin/criorchestrator/features/resume/publicapi/ExampleInstrumentedTest.kt
+++ features/resume/public-api/src/androidTest/kotlin/uk/gov/onelogin/criorchestrator/features/resume/publicapi/ExampleInstrumentedTest.kt
@@ -2,9 +2,11 @@ package uk.gov.onelogin.criorchestrator.features.resume.publicapi
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@Ignore("Placeholder test")
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
