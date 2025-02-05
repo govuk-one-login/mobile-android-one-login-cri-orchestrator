38a604e08062798acd54f4d7eb2ef4c00054b684
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-02-05 09:57:19 +0000

    test: move ProveYourIdentityUiCardTest to test source set
    - make it work with annotation to run with Android JUnit 4
    
    Resolves: DCMAW-8694

--- features/resume/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/ProveYourIdentityUiCardTest.kt
+++ features/resume/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/ProveYourIdentityUiCardTest.kt
@@ -6,10 +6,13 @@ import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.performClick
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class ProveYourIdentityUiCardTest {
     @get:Rule
     val composeTestRule = createComposeRule()
