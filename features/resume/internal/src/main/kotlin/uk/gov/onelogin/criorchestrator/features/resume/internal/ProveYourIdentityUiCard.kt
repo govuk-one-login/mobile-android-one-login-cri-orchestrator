894fc747eb3587affea03feaec8a00cd921fc085
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-02-05 16:15:44 +0000

    test: remove ProveYourIdentityUiCard test tag

--- features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/ProveYourIdentityUiCard.kt
+++ features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/ProveYourIdentityUiCard.kt
@@ -3,7 +3,6 @@ package uk.gov.onelogin.criorchestrator.features.resume.internal
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import uk.gov.android.ui.componentsv2.GdsCard
@@ -18,12 +17,10 @@ internal fun ProveYourIdentityUiCard(
         title = stringResource(R.string.start_id_check_title),
         body = stringResource(R.string.start_id_check_content),
         buttonText = stringResource(R.string.start_id_check_primary_button),
-        modifier = modifier.testTag(PROVE_YOUR_IDENTITY_UI_CARD_TEST_TAG),
+        modifier = modifier,
         onClick = onStartClick,
     )
 }
-
-const val PROVE_YOUR_IDENTITY_UI_CARD_TEST_TAG = "ProveYourIdentityUiCard"
 
 @Composable
 @PreviewLightDark
