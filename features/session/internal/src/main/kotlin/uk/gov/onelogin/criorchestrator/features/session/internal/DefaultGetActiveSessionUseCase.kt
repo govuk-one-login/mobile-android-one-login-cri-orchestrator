f331b116dc44e2333fe368d1174e3eec6213847a
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-02-14 13:01:10 +0000

    fix: rebase issues

--- features/session/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/DefaultGetActiveSessionUseCase.kt
+++ features/session/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/DefaultGetActiveSessionUseCase.kt
@@ -3,8 +3,6 @@ package uk.gov.onelogin.criorchestrator.features.session.internal
 import com.squareup.anvil.annotations.ContributesBinding
 import uk.gov.android.network.api.ApiResponse
 import uk.gov.onelogin.criorchestrator.features.session.internalapi.GetActiveSessionUseCase
-import uk.gov.onelogin.criorchestrator.features.session.internalapi.SessionApi
-import uk.gov.onelogin.criorchestrator.features.session.internalapi.SessionStore
 import uk.gov.onelogin.criorchestrator.libraries.di.scopes.ActivityScope
 import uk.gov.onelogin.criorchestrator.libraries.di.scopes.CriOrchestratorScope
 import javax.inject.Inject
@@ -12,22 +10,22 @@ import javax.inject.Inject
 @ActivityScope
 @ContributesBinding(CriOrchestratorScope::class)
 class DefaultGetActiveSessionUseCase
-    @Inject
-    constructor(
-        private val sessionApi: SessionApi,
-        private val sessionStore: SessionStore,
-    ) : GetActiveSessionUseCase {
-        override suspend fun execute(): Boolean {
-            val isActiveSession: Boolean =
-                when (sessionApi.getActiveSession()) {
-                    is ApiResponse.Success<*> -> true
+@Inject
+constructor(
+    private val sessionApi: SessionApi,
+    private val sessionStore: SessionStore,
+) : GetActiveSessionUseCase {
+    override suspend fun execute(): Boolean {
+        val isActiveSession: Boolean =
+            when (sessionApi.getActiveSession()) {
+                is ApiResponse.Success<*> -> true
 
-                    is ApiResponse.Failure -> false
-                    ApiResponse.Loading -> false
-                    ApiResponse.Offline -> false
-                    else -> false
-                }
-            sessionStore.write(isActiveSession)
-            return sessionStore.read().value
-        }
+                is ApiResponse.Failure -> false
+                ApiResponse.Loading -> false
+                ApiResponse.Offline -> false
+                else -> false
+            }
+        sessionStore.write(isActiveSession)
+        return sessionStore.read().value
     }
+}
