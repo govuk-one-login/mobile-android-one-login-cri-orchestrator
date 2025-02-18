93492efc179ad6a26bfbe28d6659d20aa1725663
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-02-18 15:00:59 +0000

    wip: progressing with session api

--- features/session/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/response/ActiveSessionErrorResponse.kt
+++ features/session/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/response/ActiveSessionErrorResponse.kt
@@ -2,21 +2,17 @@ package uk.gov.onelogin.criorchestrator.features.session.internal.response
 
 enum class ActiveSessionErrorResponse {
     SERVER_ERROR,
-    INVALID_SESSION,
     SESSION_NOT_FOUND,
     INVALID_REQUEST,
     UNAUTHORIZED,
     UNKNOWN,
-    ;
-
-    fun mapResponseToError(response: String) {
-        when (response) {
-            "server_error" -> SERVER_ERROR
-            "invalid_session" -> INVALID_SESSION
-            "session_not_found" -> SESSION_NOT_FOUND
-            "invalid_request" -> INVALID_REQUEST
-            "unauthorized" -> UNAUTHORIZED
-            else -> UNKNOWN
-        }
-    }
 }
+
+fun mapErrorStatusCodeToError(statusCode: Int): ActiveSessionErrorResponse =
+    when (statusCode) {
+        400 -> ActiveSessionErrorResponse.INVALID_REQUEST
+        401 -> ActiveSessionErrorResponse.UNAUTHORIZED
+        404 -> ActiveSessionErrorResponse.SESSION_NOT_FOUND
+        500 -> ActiveSessionErrorResponse.SERVER_ERROR
+        else -> ActiveSessionErrorResponse.UNKNOWN
+    }
--- features/session/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/SessionApiImplTest.kt
+++ features/session/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/SessionApiImplTest.kt
@@ -1,6 +1,8 @@
 package uk.gov.onelogin.criorchestrator.features.session.internal
 
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import uk.gov.android.network.api.ApiResponse
@@ -12,7 +14,13 @@ class SessionApiImplTest {
     private val sessionApiImpl =
         SessionApiImpl(
             StubHttpClient(
-                ApiResponse.Success<String>("Test"),
+                ApiResponse.Success<String>("""
+                        {
+                            "sessionId": "37aae92b-a51e-4f68-b571-8e455fb0ec34",
+                            "redirectUri": "https://example/redirect",
+                            "state": "11112222333344445555666677778888"
+                        }
+                """.trimIndent()),
             ),
         )
 
