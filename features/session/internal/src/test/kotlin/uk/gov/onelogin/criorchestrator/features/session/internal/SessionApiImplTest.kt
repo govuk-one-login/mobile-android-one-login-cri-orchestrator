08ce13677c1806b6abbf38644361b762a234ba13
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-02-18 10:10:35 +0000

    docs: move comment to correct place in SessionApiImplTest

--- features/session/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/SessionApiImplTest.kt
+++ features/session/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/SessionApiImplTest.kt
@@ -7,6 +7,8 @@ import uk.gov.android.network.api.ApiResponse
 import uk.gov.android.network.client.StubHttpClient
 
 class SessionApiImplTest {
+    // DCMAW-10105: stubbed responses in tests should reflect what we expect the API to return so
+    // that we can test how the SessionApiImpl handles and maps different types of responses
     private val sessionApiImpl =
         SessionApiImpl(
             StubHttpClient(
@@ -14,8 +16,6 @@ class SessionApiImplTest {
             ),
         )
 
-    // DCMAW-10105: stubbed responses in tests should reflect what we expect the API to return so
-    // that we can test how the SessionApiImpl handles and maps different types of responses
     @Test
     fun `session API implementation returns stubbed API response`() {
         val expected = ApiResponse.Success("Test")
