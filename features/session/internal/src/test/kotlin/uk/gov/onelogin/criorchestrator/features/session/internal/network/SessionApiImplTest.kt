91f8a678bbd62887bcd1632384a1cb0530c1bbb2
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-02-19 13:54:40 +0000

    fix: :features:session tests

--- features/session/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/network/SessionApiImplTest.kt
+++ features/session/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/network/SessionApiImplTest.kt
@@ -7,15 +7,13 @@ import uk.gov.android.network.api.ApiResponse
 import uk.gov.android.network.client.StubHttpClient
 
 class SessionApiImplTest {
-    // DCMAW-10105: stubbed responses in tests should reflect what we expect the API to return so
-    // that we can test how the SessionApiImpl handles and maps different types of responses
     private val sessionApiImpl =
         SessionApiImpl(
             StubHttpClient(
                 ApiResponse.Success<String>(
                     """
                     {
-                        "sessionId": "37aae92b-a51e-4f68-b571-8e455fb0ec34",
+                        "sessionId": "test session ID",
                         "redirectUri": "https://example/redirect",
                         "state": "11112222333344445555666677778888"
                     }
@@ -26,7 +24,17 @@ class SessionApiImplTest {
 
     @Test
     fun `session API implementation returns stubbed API response`() {
-        val expected = ApiResponse.Success("Test")
+        val expected =
+            ApiResponse.Success<String>(
+                """
+                {
+                    "sessionId": "test session ID",
+                    "redirectUri": "https://example/redirect",
+                    "state": "11112222333344445555666677778888"
+                }
+                """.trimIndent(),
+            )
+
         val result =
             runBlocking {
                 return@runBlocking sessionApiImpl.getActiveSession()
