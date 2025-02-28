4c59692763badb499fca33683cf2ff5e04d15688
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-02-28 09:34:05 +0000

    test: add screenshot test for dev menu

--- test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/devmenu/DevMenu.kt
+++ test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/devmenu/DevMenu.kt
@@ -15,11 +15,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import uk.gov.android.ui.pages.dialog.FullScreenDialog
+import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.logging.api.Logger
+import uk.gov.logging.testdouble.SystemLogger
 import uk.gov.onelogin.criorchestrator.features.config.publicapi.ConfigField
 import uk.gov.onelogin.criorchestrator.features.config.publicapi.ConfigProvider
 import uk.gov.onelogin.criorchestrator.features.config.publicapi.ConfigStore
+import uk.gov.onelogin.criorchestrator.features.config.publicapi.InMemoryConfigStore
+import uk.gov.onelogin.criorchestrator.testwrapper.R
 
 @Composable
 @Suppress("LongMethod")
@@ -54,9 +60,9 @@ internal fun DevMenu(
     ) {
         Column(
             modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
+            Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
             verticalArrangement = Arrangement.SpaceBetween,
         ) {
             TextField(
@@ -75,8 +81,8 @@ internal fun DevMenu(
 
             Row(
                 modifier =
-                    Modifier
-                        .align(Alignment.Start),
+                Modifier
+                    .align(Alignment.Start),
                 verticalAlignment = Alignment.Bottom,
             ) {
                 Button(
@@ -93,5 +99,27 @@ internal fun DevMenu(
                 }
             }
         }
+    }
+}
+
+@Composable
+@PreviewLightDark
+internal fun DevMenuPreview() {
+    val logger = SystemLogger()
+    val configStore = InMemoryConfigStore(logger)
+    configStore.write(
+        ConfigProvider(
+            backendAsyncUrl = LocalContext.current.resources.getString(
+                R.string.backendAsyncUrl
+            )
+        )
+    )
+    GdsTheme {
+        DevMenu(
+            state = DevMenuState(true),
+            configStore = configStore,
+            logger = logger,
+            modifier = Modifier,
+        )
     }
 }
